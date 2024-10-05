9ec1b257cf3892020d942e500f1767b839ddd6d4
aliyukamilu <aliyukamilu002@gmail.com>
2024-10-05 08:50:55 +0100

    chnaged somethings on login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -79,7 +79,11 @@ const Login = () => {
     try {
       const response = await fetch(`${HOST}/User/SignIn?type=email`, {
         method: "POST",
+<<<<<<< HEAD
         headers: { "Content-Type": "application/json" },
+=======
+        headers: { 'Content-Type': 'application/json' },
+>>>>>>> 407cc11 (chnaged somethings on login page)
         body: JSON.stringify(data),
       });
       const responseData = await response.json();
@@ -89,6 +93,7 @@ const Login = () => {
         dispatch(setUser(responseData.data));
         dispatch(setAccessToken(responseData.data.accessToken));
 
+<<<<<<< HEAD
         // console.log(responseData);
         // if (!responseData.data.username || responseData.data.username === " ") {
         //   setOtpRequired(true);
@@ -100,12 +105,124 @@ const Login = () => {
         setOtpRequired(true);
       } else {
         toast.error("Login failed. Please try again.");
+=======
+      if (responseData.success) {
+        dispatch(setUser(responseData.data));
+        dispatch(setAccessToken(responseData.data.accessToken));
+
+        if (!responseData.data.username || responseData.data.username === " ") {
+          setOtpRequired(true);
+        } else {
+          localStorage.setItem('freeTrialCount', '10');
+          router.push("/app");
+        }
+      } else {
+        toast.error('Login failed. Please try again.');
+      }
+    } catch (error) {
+      dismissLoader();
+      toast.error('Something went wrong. Please try again.');
+    }
+  };
+
+  // Handle OTP verification
+  const onOtpSubmit = async (data) => {
+    const completeOtp = otp.join("");
+    if (completeOtp.length < 4) {
+      toast.error("Please enter all 4 digits");
+    } else {
+      showLoader();
+      try {
+        const otpResponse = await fetch(`${HOST}/User/Verify`, {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ userId: userData.id, otp: completeOtp }),
+        });
+
+        const otpResponseData = await otpResponse.json();
+        dismissLoader();
+
+        if (otpResponseData.success) {
+          if (!otpResponseData.data.username || otpResponseData.data.username === " ") {
+            dispatch(setUsernameAvailability(null));
+            dispatch(setAccessToken(otpResponseData.data.accessToken));
+            setUsernameRequired(true);
+            setOtpRequired(false);
+            toast.success('OTP verified. Please set your username.');
+          } else {
+            localStorage.setItem('freeTrialCount', '10');
+            router.push("/app");
+          }
+        } else {
+          toast.error('OTP verification failed.');
+        }
+      } catch (error) {
+        dismissLoader();
+        toast.error('Something went wrong.');
+      }
+    }
+  };
+
+  // Handle username availability check
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (watch("username")) {
+        checkUsernameAvailability(watch("username"));
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [watch("username")]);
+
+  const checkUsernameAvailability = async (username) => {
+    dispatch(setCheckingUsername(true));
+    try {
+      const response = await fetch(`${HOST}/User/Profile/CheckUsername`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": `Bearer ${accessToken}` },
+        body: JSON.stringify({ username }),
+      });
+
+      const data = await response.json();
+      if (data.success) {
+        dispatch(setUsernameAvailability(true));
+      } else {
+        dispatch(setUsernameAvailability(false));
+      }
+    } catch (error) {
+      console.log("Error checking username", error);
+    } finally {
+      dispatch(setCheckingUsername(false));
+    }
+  };
+
+  const onUsernameSubmit = async (data) => {
+    showLoader();
+    try {
+      const usernameResponse = await fetch(`${HOST}/User/Profile/UpdateUsername`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json", "Authorization": `Bearer ${accessToken}` },
+        body: JSON.stringify({ username: data.username }),
+      });
+
+      const usernameResponseData = await usernameResponse.json();
+      dismissLoader();
+
+      if (usernameResponseData.success) {
+        toast.success('Username set successfully!');
+        dispatch(setUser(usernameResponseData.data));
+        localStorage.setItem('freeTrialCount', '10');
+        router.push("/app");
+      } else {
+        toast.error('Failed to set username.');
+>>>>>>> 407cc11 (chnaged somethings on login page)
       }
     } catch (error) {
       dismissLoader();
       toast.error("Something went wrong. Please try again.");
     }
   };
+<<<<<<< HEAD
 
   // Handle OTP verification
   const onOtpSubmit = async (data) => {
@@ -219,17 +336,16 @@ const Login = () => {
       toast.error("Something went wrong.");
     }
   };
+=======
+>>>>>>> 407cc11 (chnaged somethings on login page)
   return (
-    <div className="flex justify-center items-center h-[100vh] text-white">
-      <div className="flex flex-col gap-6">
-        <div className="grid gap-2">
-          <h1 className="text-2xl font-semibold">Login</h1>
-          <h2 className="font-clash font-medium">Welcome Back 👋</h2>
-          <p className="text-balance text-white font-quicksand">
-            Login to your account and start creating your content.
-          </p>
-        </div>
+    <div>
+      <div className='bg-[orange]  p-2 w-full'>
+        <p className='text-center text-sm text-[black]'>Please note: This is on a free version server, loading time might slow.</p>
+      </div>
+      <div className="flex justify-center items-center h-[100vh] text-white">
 
+<<<<<<< HEAD
         {/* Username Setup Form */}
         {usernameRequired ? (
           <form
@@ -291,64 +407,123 @@ const Login = () => {
           >
             <div className="flex gap-2">
               {otp.map((_, index) => (
+=======
+        <div className="flex flex-col gap-6">
+          <div className="grid gap-2">
+            <h1 className="text-2xl font-semibold">Login</h1>
+            <h2 className="font-clash font-medium">Welcome Back 👋</h2>
+            <p className="text-balance text-white font-quicksand">
+              Login to your account and start creating your content.
+            </p>
+          </div>
+
+          {/* Username Setup Form */}
+          {usernameRequired ? (
+            <form onSubmit={handleSubmit(onUsernameSubmit)} className="flex flex-col gap-4">
+              <div className="relative flex items-center w-full">
+>>>>>>> 407cc11 (chnaged somethings on login page)
                 <Input
-                  key={index}
+                  className={`bg-[#252C33] h-[52px] text-lg mt-5 w-full pr-10 ${usernameAvailable === false && !isCheckingUsername && watch("username") ? "border-red-500" : ""
+                    } ${usernameAvailable === true && !isCheckingUsername && watch("username") ? "border-green-500" : ""
+                    }`}
                   type="text"
-                  maxLength="1"
-                  value={otp[index]}
-                  onChange={(e) => handleOtpChange(e, index)}
-                  onKeyDown={(e) => handleKeyDown(e, index)}
-                  ref={(el) => (inputRefs.current[index] = el)}
-                  className="w-12 h-12 text-center text-2xl border rounded-md"
+                  placeholder="Set your username"
+                  {...register("username")}
                 />
-              ))}
-            </div>
 
-            <Button
-              className="main-gradient inline-block rounded-lg w-full my-5 px-8 py-3 text-center font-semibold tracking-tight !text-white transition duration-200 hover:font-bold bg-gradient-to-tr from-yellow-400 to-orange-600"
-              type="submit"
-            >
-              Verify OTP
-            </Button>
-          </form>
-        ) : (
-          // Email Login Form
-          <form onSubmit={handleSubmit(onEmailSubmit)}>
-            <Input
-              className="bg-[#252C33] h-[52px] text-lg mt-5"
-              type="email"
-              placeholder="Username or email"
-              {...register("email")}
-            />
-            <Button
-              className="main-gradient inline-block rounded-lg w-full my-5 px-8 py-3 text-center font-semibold tracking-tight !text-white transition duration-200 hover:font-bold bg-gradient-to-tr from-yellow-400 to-orange-600"
-              type="submit"
-            >
-              Continue
-            </Button>
+                {watch("username") && isCheckingUsername && (
+                  <Loader className="absolute right-2 top-8 text-sm text-gray-500" />
+                )}
+                {watch("username") && usernameAvailable === false && !isCheckingUsername && (
+                  <X className="absolute right-2 top-8 text-sm text-red-500" />
+                )}
+                {watch("username") && usernameAvailable === true && !isCheckingUsername && (
+                  <Check className="absolute right-2 top-8 text-green-500" />
+                )}
+              </div>
 
-            <div className="flex items-center gap-3">
+              <Button
+                className="main-gradient inline-block rounded-lg w-full my-5 px-8 py-3 text-center font-semibold tracking-tight !text-white transition duration-200 hover:font-bold bg-gradient-to-tr from-yellow-400 to-orange-600"
+                type="submit"
+                disabled={isCheckingUsername || usernameAvailable === false || !watch("username")} // Disable if username is not available, still checking, or empty
+              >
+                Set Username
+              </Button>
+            </form>
+
+
+          ) : otpRequired ? (
+            // OTP Verification Form
+            <form onSubmit={handleSubmit(onOtpSubmit)} className="flex flex-col gap-4">
+
+              <div className="flex gap-2">
+                {otp.map((_, index) => (
+                  <Input
+                    key={index}
+                    type="text"
+                    maxLength="1"
+                    value={otp[index]}
+                    onChange={(e) => handleOtpChange(e, index)}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    ref={(el) => (inputRefs.current[index] = el)}
+                    className="w-12 h-12 text-center text-2xl border rounded-md"
+                  />
+                ))}
+              </div>
+
+              <Button
+                className="main-gradient inline-block rounded-lg w-full my-5 px-8 py-3 text-center font-semibold tracking-tight !text-white transition duration-200 hover:font-bold bg-gradient-to-tr from-yellow-400 to-orange-600"
+                type="submit"
+              >
+                Verify OTP
+              </Button>
+            </form>
+          ) : (
+            // Email Login Form
+            <form onSubmit={handleSubmit(onEmailSubmit)}>
+              <Input
+                className="bg-[#252C33] h-[52px] text-lg mt-5"
+                type="email"
+                placeholder="Username or email"
+                {...register("email")}
+              />
+              <Button
+                className="main-gradient inline-block rounded-lg w-full my-5 px-8 py-3 text-center font-semibold tracking-tight !text-white transition duration-200 hover:font-bold bg-gradient-to-tr from-yellow-400 to-orange-600"
+                type="submit"
+              >
+                Continue
+              </Button>
+
+              {/* <div className="flex items-center gap-3">
               <div className="border flex-1 border-[#a7a7a7]"></div>
               <div>OR</div>
               <div className="border flex-1 border-[#a7a7a7]"></div>
-            </div>
+            </div> */}
 
-            {/* <WalletConnection /> */}
+              {/* <WalletConnection /> */}
 
-            {/* <Button
+              {/* <Button
               type="button"
               className="main-gradient inline-block rounded-lg w-full my-5 px-8 py-3 text-center font-semibold tracking-tight !text-white transition duration-200 hover:font-bold bg-gradient-to-tr from-yellow-400 to-orange-600"
               onClick={() => document.querySelector('.wallet-adapter-button-trigger').click()} // Trigger WalletMultiButton onClick
             >
               Connect Wallet
             </Button> */}
+<<<<<<< HEAD
             <WalletMultiButton style={{}} />
           </form>
         )}
-      </div>
+=======
+              {/* <WalletMultiButton style={{}} /> */}
 
-      {/* React Toastify Container */}
-      <ToastContainer />
+            </form>
+          )}
+        </div>
+
+        {/* React Toastify Container */}
+        <ToastContainer />
+>>>>>>> 407cc11 (chnaged somethings on login page)
+      </div>
     </div>
   );
 };
